806108bcb0e69afb59167fa324f9d9b3e47e1fca
TADA Tadashi <t@tdtds.jp>
2011-12-02 17:50:46 +0900

    added methods command and run into Kindlegen module.

--- lib/kindlegen.rb
+++ lib/kindlegen.rb
@@ -1,5 +1,26 @@
 require "kindlegen/version"
+require 'pathname'
 
 module Kindlegen
-  # Your code goes here...
+	#
+	# Getting command path of kindlegen.
+	# If it required under gem, returning path in the extension.
+	# Not if, it return simply 'kindlegen' so you have to copy the command into $PATH.
+	#
+	def self.command
+		if __FILE__.include? 'gems'
+			(Pathname( __FILE__ ).parent + '../ext/kindlegen/kindlegen').to_s
+		else
+			'kindlegen'
+		end
+	end
+
+	#
+	# Run kindlegen command with spacified parameters
+	#
+	# _params_:: array of command parameters.
+	#
+	def self.run( *params )
+		system command, *params
+	end
 end
