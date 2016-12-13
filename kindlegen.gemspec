e9d7c8860a1cd561cb52754fd8270f5d545204db
TADA, Tadashi <t@tdtds.jp>
2016-12-13 16:10:54 +0900

    add kindlegen command written by ruby
    * add kindlegen command written by ruby calling original (binary) kindlegen command
    * update unit tests to delete executable without Yes/No
    * support both new/old Gem::Installer in unit tests
    * update support rubys on TravisCI

--- kindlegen.gemspec
+++ kindlegen.gemspec
@@ -17,7 +17,8 @@ Gem::Specification.new do |s|
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.bindir        = 'exe'
+  s.executables   = s.files.grep(%r{^exe/}){|f| File.basename(f)}
   s.require_paths = ["lib"]
   s.extensions    = ['ext/Rakefile']
 
--- test/test_kindlegen.rb
+++ test/test_kindlegen.rb
@@ -9,7 +9,7 @@ class KindlegenTest < Test::Unit::TestCase
     kindlegen_lib_dir = nil
     gem_version = File.read(File.join(KINDLEGEN_PROJECT_DIR, 'lib/kindlegen/version.rb')).match(/VERSION = ["'](.*?)["']/)[1]
     gem_file = File.join(KINDLEGEN_PROJECT_DIR, 'pkg', %(kindlegen-#{gem_version}.gem))
-    result = Gem::Installer.new(gem_file).install
+	 result = Gem::Installer.at(gem_file).install rescue Gem::Installer.new(gem_file).install
     begin
       require 'kindlegen'
     rescue ::LoadError
@@ -21,7 +21,7 @@ class KindlegenTest < Test::Unit::TestCase
     output = %x(#{Kindlegen.command})
     assert output.include?('Amazon')
   ensure
-    Gem::Uninstaller.new('kindlegen', :force => true).uninstall rescue nil
+    Gem::Uninstaller.new('kindlegen', :executables => true).uninstall rescue nil
     $:.delete kindlegen_lib_dir if kindlegen_lib_dir
   end
 end
